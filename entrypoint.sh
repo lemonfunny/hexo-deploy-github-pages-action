06c462dbf49443e3e91679038daa13b3c40aed11
lemonbuluo <liuguiyang6559@gmail.com>
2022-02-09 20:45:30 +0800

    add aciton

---
+++ entrypoint.sh
@@ -0,0 +1,78 @@
+#!/bin/sh -l
+
+set -e
+
+# check values
+
+if [ -n "${PUBLISH_REPOSITORY}" ]; then
+    TARGET_REPOSITORY=${PUBLISH_REPOSITORY}
+else
+    TARGET_REPOSITORY=${GITHUB_REPOSITORY}
+fi
+
+if [ -n "${BRANCH}" ]; then
+    TARGET_BRANCH=${BRANCH}
+else
+    TARGET_BRANCH="gh-pages"
+fi
+
+if [ -n "${PUBLISH_DIR}" ]; then
+    TARGET_PUBLISH_DIR=${PUBLISH_DIR}
+else
+    TARGET_PUBLISH_DIR="./public"
+fi
+
+if [ -z "$PERSONAL_TOKEN" ]
+then
+  echo "You must provide the action with either a Personal Access Token or the GitHub Token secret in order to deploy."
+  exit 1
+fi
+
+REPOSITORY_PATH="https://x-access-token:${PERSONAL_TOKEN}@github.com/${TARGET_REPOSITORY}.git"
+
+# deploy to
+echo ">>>>> Start deploy to ${TARGET_REPOSITORY} <<<<<"
+
+# Installs Git.
+echo ">>> Install Git ..."
+apt-get update && \
+apt-get install -y git && \
+
+echo ">>> Install Themes with Dependencies ..."
+cd "${GITHUB_WORKSPACE}"/themes
+git clone -b master ${THEME_REPOSITORY} ${THEME_DIRNAME}
+
+# Directs the action to the the Github workspace.
+cd "${GITHUB_WORKSPACE}"
+
+echo ">>> Install NPM dependencies ..."
+npm install
+#npm install hexo-theme-aurora --save
+
+echo ">>> Clean folder ..."
+npx hexo clean
+
+echo ">>> Generate file ..."
+npx hexo generate
+
+cd $TARGET_PUBLISH_DIR
+
+echo ">>> Config git ..."
+
+# Configures Git.
+git init
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git remote add origin "${REPOSITORY_PATH}"
+
+git checkout --orphan $TARGET_BRANCH
+
+git add .
+
+echo '>>> Start Commit ...'
+git commit --allow-empty -m "Building and deploying Hexo project from Github Action"
+
+echo '>>> Start Push ...'
+git push -u origin "${TARGET_BRANCH}" --force
+
+echo ">>> Deployment successful!"
