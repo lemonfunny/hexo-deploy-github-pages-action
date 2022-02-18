3741bf3353b2271354614147987de150fef2163c
柠萌 <66536447+lemonbuluo@users.noreply.github.com>
2022-02-19 07:52:39 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,6 +47,8 @@ cd "${GITHUB_WORKSPACE}"
 
 echo ">>> Install NPM dependencies ..."
 npm install
+#new add
+npm audit fix --force
 #npm install hexo-theme-aurora --save
 
 echo ">>> Clean folder ..."
